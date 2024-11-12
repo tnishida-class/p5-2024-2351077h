c66431fc605a6ea188b19b01ed7963a1342ee87f
2351077h <2351077h@stu.kobe-u.ac.jp>
2024-11-12 11:11:34 +0900

    Change section 5-2

--- section5-2/sketch.js
+++ section5-2/sketch.js
@@ -1,12 +1,14 @@
 // テキスト「関数を作る(1) 引数のある関数」
 // 練習問題：星を描く関数を改造して正N角形を描画する関数を作ってみよう
 function setup(){
-  createCanvas(300, 100);
+  createCanvas(400, 100);
   background(200);
   fill(0);
-  crossmark(10, 10, 90, 90);
+  crossmark(6, 10, 90, 90);
   ngmark(150, 50, 80);
   star(250, 50, 40);
+  regularPolygon(6, 350, 50, 40);
+
 }
 
 function crossmark(x1, y1, x2, y2){
